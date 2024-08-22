fa9ff017daa46060bb9131dbdb25fe36220a7257
Michael Huang <z996345@gmail.com>
2024-08-22 15:53:25 +0800

    git init

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 3000, // 更改這裡的端口號
+    host: '0.0.0.0' // 確保伺服器可以從外部訪問
+  }
+})
